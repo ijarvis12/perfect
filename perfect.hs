13e8b29696238deb28db7c552285fa5bb7594820
Ian Jarvis <ijarvis12@gmail.com>
2022-01-09 05:55:49 -0600

    indentation

--- perfect.hs
+++ perfect.hs
@@ -4,31 +4,31 @@
 
 test :: Integer -> Integer
 test y = do
-    let m = map (y `mod`) [1 .. (div y 2)]
-    let f = zip [1..] m
-    let fltr = fst (unzip (filter ((==0).snd) f))
-    sum fltr
+  let m = map (y `mod`) [1 .. (div y 2)]
+  let f = zip [1..] m
+  let fltr = fst (unzip (filter ((==0).snd) f))
+  sum fltr
     
 
 perfect :: Integer -> String
 perfect x = do
-    let y = 2^(x)*(2^(x+1)-1)
-    let z = test y
-    if y == z then
-        show y
-    else []
+  let y = 2^(x)*(2^(x+1)-1)
+  let z = test y
+  if y == z then
+      show y
+  else []
 
 loop :: Integer -> IO ()
 loop x = do
-        let perf = perfect x
-        if not (null perf) then do
-            putStrLn perf
-            loop (x+1)
-        else
-            loop (x+1)
+  let perf = perfect x
+  if not (null perf) then do
+      putStrLn perf
+      loop (x+1)
+  else
+      loop (x+1)
 
 main :: IO ()
 main = do
-    putStrLn "Computing perfect numbers: "
-    let x = 1 :: Integer
-    loop x
+  putStrLn "Computing perfect numbers: "
+  let x = 1 :: Integer
+  loop x
