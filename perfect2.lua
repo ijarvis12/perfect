1d69dbb7527ea50cf24a5e5ae5900d6ff87ebddc
Ian Jarvis <ijarvis12@gmail.com>
2022-03-23 10:02:25 -0500

    comments

--- perfect2.lua
+++ perfect2.lua
@@ -10,11 +10,11 @@ print(" This program finds perfect numbers using Mersenne Primes ")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("")
 
---[[ variable maxn is the maximum number to search to --]]
+-- variable maxn is the maximum number to search to
 print("Enter max exponent #: ")
 maxn = io.read()
 
---[[ if maxn exists, attempt to make it an integer, else exit --]]
+-- if maxn exists, attempt to make it an integer, else exit
 if #maxn > 0 then
     local function maxnToNum()
         maxn = tonumber(maxn)
@@ -30,7 +30,7 @@ else
     os.exit()
 end
 
---[[ check if maxn is sane --]]
+-- check if maxn is sane
 if maxn <= 1 then
     print("Bad input")
     print("Press <Enter> to end program")
@@ -38,16 +38,16 @@ if maxn <= 1 then
     os.exit()
 end
 
---[[ find the perfect numbers --]]
+-- find the perfect numbers
 for p = 1,maxn,1 do
     psum = 0
---[[   the potential perfect number --]]
+--  the potential perfect number
     perfect = 2^(p)*(2^(p+1)-1)
 
---[[   the limit to search to --]]
+--  the limit to search to
     sqrtp = math.ceil(math.sqrt(perfect))
 
---[[   add up all the divisors into psum --]]
+--  add up all the divisors into psum
     for n = 1,sqrtp,1 do
         if perfect % n == 0 then
             psum = psum + n
@@ -55,16 +55,17 @@ for p = 1,maxn,1 do
         end
     end
 
---[[   get rid of possible extra summation --]]
+--  get rid of possible extra summation
     if sqrtp^2 == perfect then
         psum = psum - sqrtp
     end
 
---[[   if psum is equal to the potenial perfect number, we have a match --]]
+--  if psum is equal to the potenial perfect number, we have a match
     if psum == 2*perfect then
         print(perfect)
     end
 end
 
+-- end program
 print("Press <Enter> to end program")
 _ = io.read()
