7702fab9fb62b4eb31285a235e200cc4bdf5f2d6
Ian Jarvis <ijarvis12@gmail.com>
2022-11-12 19:39:09 -0600

    made concise

--- perfect.hs
+++ perfect.hs
@@ -2,52 +2,51 @@
 
 --program that finds perfect numbers using Mersenne Primes
 
-forLoop :: Integer -> [Integer] -> Integer -> Integer
-forLoop y lst w = do
-  if (w > ((ceiling (sqrt (fromIntegral y))) + 1)) then
-    sum lst
-  else do
-    let m = mod y w
-    if (m == 0) then do
-      let n = div y w
-      forLoop y (lst ++ [w,n]) (w+1)
-    else
-      forLoop y lst (w+1)
+import Control.Monad
 
-perfect :: Integer -> String
-perfect x = do
-  let y = 2^(x-1)*(2^(x)-1)
-  let z = forLoop y [1] 2
-  if y == z then
-    show y
-  else []
+check :: Integer -> Integer -> Bool
+check y w = (mod y w) == 0
+
+forLoop :: Integer -> [Integer] -> Bool -> Integer -> Integer
+forloop y lst True 1 = toInteger (sum lst)
+forLoop y lst False 1 = toInteger (sum lst)
+forLoop y lst False w = do
+    let b = check y (w-1)
+    forLoop y lst b (w-1)
+forLoop y lst True w = do
+    let n = div y w
+    let b = check y (w-1)
+    forLoop y (lst ++ [w,n]) b (w-1)
+
+perfect :: Integer -> Integer -> Bool
+perfect x m = do
+  let y = (2^(x-1)) * m
+  let end = toInteger (ceiling (sqrt (fromIntegral y)))
+  let b = check y end
+  let z = forLoop y [1] b end
+  y == z
 
 -- Lucas-Lehmer prime test for odd x > 2
-llt :: Integer -> Integer -> Integer -> Bool
-llt i s x = do
-    if i == (x-2) then
-      True
-    else do
-      let m = 2^x - 1
-      let y = ((s*s)-2) `mod` m
-      if y == 0 then
-        False
-      else
-        llt (i+1) y x
+llt :: Integer -> Integer -> Integer -> Bool -> Bool
+llt s m 0 False = False
+llt s m end True = True
+llt s m end False = do
+    let y = ((s*s)-2) `mod` m
+    llt y m (end-1) (y==m)
 
-loop :: Integer -> IO ()
-loop x = do
-  if (llt 0 4 x) then
-    loop (x+2)
-  else do
-    let perf = perfect x
-    if not (null perf) then do
-      putStrLn perf
-      loop (x+2)
-    else
-      loop (x+2)
+loop :: Integer -> Bool -> IO ()
+loop x True = do
+    print (2^(x-1)*(2^(x)-1))
+    loop (x+2) False
+loop x False = do
+    let m = 2^x - 1
+    let l = llt 4 m (m-2) False
+    when l $ loop (x+2) False
+    let perf = perfect x m
+    when perf $ loop x True
+    loop (x+2) False
 
 main :: IO ()
 main = do
   putStrLn "Computing perfect numbers: \n6\n28"
-  loop 5
+  loop 5 True
